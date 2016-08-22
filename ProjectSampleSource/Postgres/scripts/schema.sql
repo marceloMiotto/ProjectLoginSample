4b558611d224cf6533a29bfd3c7129449b45ff84
Marcelo_Miotto <mmiotto@gmail.com>
2016-08-22 15:01:52 -0300

    initial project version

---
+++ ProjectSampleSource/Postgres/scripts/schema.sql
@@ -0,0 +1,22 @@
+/*
+** Step 1- Create Schema samples
+*/
+CREATE SCHEMA samples;
+
+
+/*
+** Step 2 - Create the table under the samples schema
+*/
+CREATE TABLE SAMPLES.USERS(USER_ID TEXT, PASSWORD TEXT);
+
+
+/*
+** Step 3 - Insert a test row
+*/
+INSERT INTO samples.users(user_id, password) values ('sample@email.com','1234');
+
+/*
+** Step 4 - Check if the user was created
+*/
+SELECT *
+  FROM samples.users;
